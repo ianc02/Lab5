bd637cbde10f68003a02c943e8d1c74837061898
ianc02 <77699025+ianc02@users.noreply.github.com>
2021-03-17 23:11:03 -0500

    Added basic enemy prefab with seeking behavior

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject player;
+    void Start()
+    {
+
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        agent.destination = player.transform.position;
+        transform.LookAt(player.transform);
+    }
+}
