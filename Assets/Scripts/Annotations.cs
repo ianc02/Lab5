827de03050e60ec0a6e8a5bb4201aa11406679f0
Cooper Grimes <grimescb@hendrix.edu>
2021-03-21 21:38:12 -0500

    Added 4 songs
    Added 4 songs we can use for the main menu and the 3 levels. They have been added to a new asset folder and the attributions script has been updated

--- Assets/Scripts/Annotations.cs
+++ Assets/Scripts/Annotations.cs
@@ -7,6 +7,17 @@ public class Annotations : MonoBehaviour
     // Start is called before the first frame update
 
     // Flashlight - RR Freelance https://poly.google.com/view/bJaT8R5j3uD
+
+    /* Music:
+     * Closing In 3 https://soundimage.org/wp-content/uploads/2021/02/Closing-In-3.mp3
+     * The Front Door of a Haunted House https://soundimage.org/wp-content/uploads/2019/10/The-Front-Door-of-a-Haunted-House.mp3
+     * Midnight Fog https://soundimage.org/wp-content/uploads/2019/07/Midnight-Fog.mp3
+     * More Sewer Creepers Looping https://soundimage.org/wp-content/uploads/2019/05/More-Sewer-Creepers_Looping.mp3
+     * 
+     * Music by Eric Matyas
+     * www.soundimage.org
+     */
+
     void Start()
     {
         
