33ae2a55127c8db8f9154ffe0bfd342c6bbd0903
thoopchuk26 <thoopchuk@gmail.com>
2021-03-21 22:16:40 -0500

    Added Playable Character Prefab with Movement and Camera Control

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f, gravity = -9.81f, jumpHeight = 3f;
+    private float x, y;
+
+    private Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * y;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
